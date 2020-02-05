94b6fc71dd14864d7c251ce3363fa72a6df19e06
Maduabuchi <maduabuchiokonkwo@gmail.com>
2020-02-05 13:38:24 +0100

    fixed image display error

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,58 +1,55 @@
 import React from 'react';
+
 import MenuItem from '../menu-items/menu-item.components';
-import './directory.styles.scss'
-class Directory extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            sections:[
-              {
-                title: 'PMS',
-                imageUrl: `url(${require('./images/PMS.jpeg')})`,
-                id: 1,
-                linkUrl: 'shop/pms'
-              },
-              {
-                title: 'LPG',
-                imageUrl: `url(${require('./images/LPG.jpg')})`,
-                id: 2,
-                linkUrl: 'shop/lpg'
-              },
-              {
-                title: 'desel',
-                imageUrl: `url(${require('./images/desel.jpeg')})`,
-                id: 3,
-                linkUrl: 'shop/desel'
-              },
-              {
-                title: 'kerosene',
-                imageUrl: `url(${require('./images/Kerosene.jpeg')})`,
-                size: 'large',
-                id: 4,
-                linkUrl: 'shop/kerosene'
-              },
-              {
-                title: 'others',
-                imageUrl: `url(${require('./images/accessories.jpeg')})`,
-                size: 'large',
-                id: 5,
-                linkUrl: 'shop/others'
-              }
-            ]
+
+import './directory.styles.scss';
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          title: 'PMS',
+          imageUrl: 'https://i.ibb.co/T84MY1K/PMS.jpg',
+          id: 1
+       },
+        {
+          title: 'kerosene',
+          imageUrl: 'https://i.ibb.co/qRKSHHn/Kerosene.jpg',
+          id: 2
+        },
+        {
+          title: 'lpg',
+          imageUrl: 'https://i.ibb.co/ckJRw27/LPG.jpg',
+          id: 3
+        },
+        {
+          title: 'diesel',
+          imageUrl: 'https://i.ibb.co/ssLMNbW/desel.jpg',
+          size: 'large',
+          id: 4
+        },
+        {
+          title: 'others',
+          imageUrl: 'https://i.ibb.co/RDxrnvB/accessories.jpg',
+          size: 'large',
+          id: 5
         }
+      ]
+    };
+  }
 
-
-
-    }
-    render(){
-        return (
-            <div className="directory-menu">
-              {this.state.sections.map(({ title, imageUrl, id, size })=>(
-                <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
-              ))}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className='directory-menu'>
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Directory;
