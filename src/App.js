caeb067d605de1bd16ad8c1f253ea5cf7b38adf2
Maduabuchi <maduabuchiokonkwo@gmail.com>
2020-02-08 03:56:52 +0100

    added react-router and updated menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Switch, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+const PmsPage = () =>(
+  <div>This is the PMS page</div>
+)
+
 function App() {
   return (
     <div>
-     <HomePage />
+    <Switch>
+    <Route exact path='/' component={HomePage} />
+    <Route exact path='/pms' component={PmsPage} />
+    </Switch>
+     
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,29 +13,34 @@ class Directory extends React.Component {
         {
           title: 'PMS',
           imageUrl: 'https://i.ibb.co/T84MY1K/PMS.jpg',
-          id: 1
+          id: 1,
+          linkUrl: 'pms'
        },
         {
           title: 'kerosene',
           imageUrl: 'https://i.ibb.co/qRKSHHn/Kerosene.jpg',
-          id: 2
+          id: 2,
+          linkUrl: ' '
         },
         {
           title: 'lpg',
           imageUrl: 'https://i.ibb.co/ckJRw27/LPG.jpg',
-          id: 3
+          id: 3,
+          linkUrl: ' '
         },
         {
           title: 'diesel',
           imageUrl: 'https://i.ibb.co/ssLMNbW/desel.jpg',
           size: 'large',
-          id: 4
+          id: 4,
+          linkUrl: ' '
         },
         {
           title: 'others',
           imageUrl: 'https://i.ibb.co/RDxrnvB/accessories.jpg',
           size: 'large',
-          id: 5
+          id: 5,
+          linkUrl: ' '
         }
       ]
     };
@@ -44,8 +49,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps}) => (
+          <MenuItem key={id} {...otherSectionProps}/>
         ))}
       </div>
     );
