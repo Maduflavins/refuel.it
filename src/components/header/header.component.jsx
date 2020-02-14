7d60bce14e3f937ba17e3cdac695c9fe0f5dd5f7
Maduabuchi <maduabuchiokonkwo@gmail.com>
2020-02-14 23:41:35 +0100

    added logo component

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './header.styles.scss';
+
+import Logo from './Logo.png';
+
+const Header = () =>(
+    <div className='header'>
+        <Link className='logo-container' to="/">
+            {/* <Logo className='logo' /> */}
+            <img src={Logo} />
+        </Link>
+        <div className='options'>
+            <Link className='option' to='/shop'>
+                shop
+            </Link>
+            <Link className='option' to='/shop'>
+                Contact
+            </Link>
+        </div>
+    </div>
+)
+
+
+export default Header;
